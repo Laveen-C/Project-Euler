c6e9e3e527913fff152d314d063081e60bf8a46a
Laveen <laveen2512@gmail.com>
2020-05-23 12:20:55 +0100

    Initial q1 files (test)

---
+++ q1/q1.cpp
@@ -0,0 +1,9 @@
+//Need to learn iteration in c++ before attempting this question.
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
