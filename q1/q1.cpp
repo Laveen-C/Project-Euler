4e238656ed18f46cf53ad9da2c4ab4c35dfbc9c2
Laveen-C <laveen2512@gmail.com>
2020-05-28 22:20:27 +0100

    Vectors in C++ test #4

--- q1/q1.cpp
+++ q1/q1.cpp
@@ -1,53 +1,15 @@
 #include <iostream>
-#include <stdlib.h>
-#include <ctime>
+#include <vector>
 
 int main() {
-    
-  srand (time(NULL));
-  int	fortune = rand() % 10;
-  std::cout << fortune << std::endl ;
 
-  if (fortune == 0) {
+  std::vector<int> yeet(12);
 
-    std::cout << "Flattery will go far tonight.\n";
+  int i;
   
-  } else if (fortune == 1) {
-    
-    std::cout << "Don't behave with cold manners.\n";
-  
-  } else if (fortune == 2) {
-    
-    std::cout << "May you someday be carbon neutral\n";
-    
-  } else if (fortune == 3) {
-    
-    std::cout << "You have rice in your teeth.\n";
-    
-  } else if (fortune == 4) {
-    
-    std::cout << "A conclusion is simply the place where you got tired of thinking.\n";
-    
-  } else if (fortune == 5) {
-    
-    std::cout << "No snowflake feels responsible in an avalanche.\n";
-    
-  } else if (fortune == 6) {
-    
-    std::cout << "He who laughs last is laughing at you.\n";
-    
-  } else if (fortune == 7) {
-    
-    std::cout << "If you look back, you'll soon be going that way.\n";
-    
-  } else if (fortune == 8) {
-    
-    std::cout << "You will die alone and poorly dressed.\n";
-      
-  } else if (fortune == 9) {
-    
-    std::cout << "The fortune you seek is in another cookie.\n";
-      
+  for (i = 0; i < yeet.size(); i++) {
+  std::cout << yeet[i] << std::endl;
   }
-  
+
+  return 0;
 }
